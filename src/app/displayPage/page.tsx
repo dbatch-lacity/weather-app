f0401a7cfac264b8ea527511407005add7a8e6a3
dbatch-lacity <daniel.batchman@lacity.org>
2023-12-05 09:53:38 -0800

    test within page added

--- src/app/displayPage/page.tsx
+++ src/app/displayPage/page.tsx
@@ -2,6 +2,7 @@
 import WeatherTable from "../components/weatherTable";
 import { useSearchParams } from "next/navigation";
 import fetchWeatherData, { WeatherInfo } from "../weatherInfo/fetchWeather";
+import { useEffect, useState } from "react";
 
 /**
  *
@@ -9,14 +10,40 @@ import fetchWeatherData, { WeatherInfo } from "../weatherInfo/fetchWeather";
  * 2. fetch data based on a passed paramater
  * 3. display the resulting data
  */
-async function weatherDisplayPage() {
+/**
+ * change it from an async component to a regular comp
+ * change results to be a state
+ * use useEffect
+ * make sure when entry is not valid to direct user to main page and show an alert saying entry is invalid
+ * @returns
+ */
+function weatherDisplayPage() {
+  const [test, setTest] = useState(0);
+
   const searchParams = useSearchParams();
-  const zipCode = searchParams?.get("zipCode") as string; //remove typecasting later on
-  const results = (await fetchWeatherData(zipCode)) as WeatherInfo;
-  console.log(results);
+  const zipCode = searchParams?.get("zipCode") as string;
+  // const results = (await fetchWeatherData(zipCode)) as WeatherInfo;
+  console.log("Weather Display page updated");
+
+  useEffect(() => {
+    setTest(test + 1);
+    console.log("test in the body");
+    return () => {
+      console.log("test was tested once");
+    };
+  }, []);
+
   return (
     <div>
-      <WeatherTable weatherInfo={results}></WeatherTable> {}
+      <button
+        onClick={() => {
+          setTest(test * 10);
+        }}
+      >
+        Button
+      </button>
+      {test}
+      {/* <WeatherTable weatherInfo={results}></WeatherTable> {} */}
     </div>
   );
 }
